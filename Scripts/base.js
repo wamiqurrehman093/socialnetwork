552d0b8fee784fc2e0e4fc07ca914641cac9076f
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2023-05-17 10:06:13 +0500

    JavaScript file cleanup

--- Scripts/base.js
+++ Scripts/base.js
@@ -1,17 +1,8 @@
-/* JQuery */
-
 $(document).ready(function () {
     setupPostSubmission();
-    setupLikeButtons();
-    setupCommentButtons();
+    setupRecentPosts();
 });
 
-const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
-
-/* Post Submission */
 function setupPostSubmission(){
     setupPostMediaFileButton();
     setupSubmitPostButton();
@@ -34,8 +25,6 @@ function setupSubmitPostButton(){
     submitPostButton.addEventListener("click", function (event) {
         event.preventDefault();
 
-        var submitPostButton = $(this);
-
         var postText = $(".submit-post-text")[0];
         var mediaFile = $(".post-media-file-button")[0];
         if (postText.value === "" && mediaFile.files.length === 0)
@@ -48,11 +37,12 @@ function setupSubmitPostButton(){
             fileName = file.name;
         }
         var dateString = getDateString();
-        var postNumber = $("#recent-posts").length + 1;
+        var postNumber = $("#recent-posts .post").length;
+        console.log($("#recent-posts .post").length);
         var data = {
             author: {name: "Naruto Uzumaki", image: "Naruto1.jpg"},
             post: {
-                date: dateString, text: postText.value, image: fileName, number: postNumber,
+                date: dateString, text: postText.value, image: fileName, number: postNumber + 1,
                 numberOfLikes: 0, numberOfComments: 0, numberOfShares: 0
             }
         };
@@ -62,21 +52,31 @@ function setupSubmitPostButton(){
         mediaFile.value = "";
         postText.value = "";
         $(".post-media-file-label p").html("Upload Media");
+        
+        setupPostControls(postNumber);
+        console.log(postNumber);
     });
 }
 
-/* Like Posts */
+function setupRecentPosts(){
+    for (var i = 0; i < $("#recent-posts .post").length; i++)
+        setupPostControls(i);
+}
 
-function setupLikeButtons() {
-    var likeButtons = $(".like-post-button");
-    for (var i = 0; i < likeButtons.length; i++) {
-        likeButtons[i].addEventListener("click", function () {
-            var likeButton = $(this);
-            var postID = "#" + likeButton.attr("data-post-id");
-            var likeLabel = $(postID + " .post-like-reaction p");
-            likeLabel.html(processLike(likeButton, likeLabel));
-        });
-    }
+function setupPostControls(postIndex){
+    setupLikeButton(postIndex);
+    setupOpenCommentSectionButton(postIndex);
+    setupSubmitCommentButton(postIndex);
+    setupCommentMediaFileButton(postIndex);
+}
+
+function setupLikeButton(buttonIndex){
+    $(".like-post-button")[buttonIndex].addEventListener("click", function () {
+        var likeButton = $(this);
+        var postID = "#" + likeButton.attr("data-post-id");
+        var likeLabel = $(postID + " .post-like-reaction p");
+        likeLabel.html(processLike(likeButton, likeLabel));
+    });
 }
 
 function processLike(likeButton, likeLabel) {
@@ -90,75 +90,62 @@ function processLike(likeButton, likeLabel) {
     }
 }
 
-/* Post Comment Control */
-
-function setupCommentButtons() {
-    setupOpenCommentSectionButtons();
-    setupCommentMediaFileButtons();
-    setupSubmitCommentButtons();
+function setupOpenCommentSectionButton(buttonIndex){
+    $(".open-comment-section")[buttonIndex].addEventListener("click", function () {
+        var postID = "#" + $(this).attr("data-post-id");
+        var commentSection = $(postID + " .comment-section");
+        commentSection.toggle();
+    });
 }
 
-function setupOpenCommentSectionButtons() {
-    var openCommentButtons = $(".open-comment-section");
-    for (var i = 0; i < openCommentButtons.length; i++) {
-        openCommentButtons[i].addEventListener("click", function () {
-            var postID = "#" + $(this).attr("data-post-id");
-            var commentSection = $(postID + " .comment-section");
-            commentSection.toggle();
-        });
-    }
+function setupCommentMediaFileButton(buttonIndex) {
+    $(".comment-media-file-button")[buttonIndex].addEventListener("change", function (event) {
+        var mediaFileButton = $(this);
+        var postID = "#" + mediaFileButton.attr("data-post-id");
+        const [file] = mediaFileButton[0].files;
+        const { name: fileName, size } = file;
+        const fileSize = (size / 1000).toFixed(2);
+        const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+        $(postID + " .comment-media-file-label p").html(fileNameAndSize);
+    });
 }
 
-function setupCommentMediaFileButtons(){
-    var mediaFileButtons = $(".comment-media-file-button");
-    for (var i = 0; i < mediaFileButtons.length; i++) {
-        var mediaFileButton = mediaFileButtons[i];
-        mediaFileButton.addEventListener("change", function (event) {
-            var mediaFileButton = $(this);
-            var postID = "#" + mediaFileButton.attr("data-post-id");
-            const [file] = mediaFileButton[0].files;
-            const { name: fileName, size } = file;
-            const fileSize = (size / 1000).toFixed(2);
-            const fileNameAndSize = `${fileName} - ${fileSize}KB`;
-            $(postID + " .comment-media-file-label p").html(fileNameAndSize);
-        });
-    }
+function setupSubmitCommentButton(buttonIndex){
+    $(".submit-comment-button")[buttonIndex].addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var submitCommentButton = $(this);
+        var postID = "#" + submitCommentButton.attr("data-post-id");
+
+        var comment_text = $(postID + " .submit-comment-text")[0];
+        var media_file = $(postID + " .comment-media-file-button")[0];
+        if (comment_text.value === "" && media_file.files.length === 0)
+            return console.log("Empty form!");
+        var fileName = "";
+        var template_id = "#comment-template";
+        if (media_file.files.length > 0) {
+            template_id = "#image-comment-template";
+            var [file] = media_file.files;
+            fileName = file.name;
+        }
+        var dateString = getDateString();
+        var data = {
+            author: { name: "Naruto Uzumaki", image: "Naruto1.jpg" },
+            comment: { date: dateString, text: comment_text.value, image: fileName }
+        };
+        var template = Handlebars.compile($(template_id).html());
+        var filled = template(data);
+        $(postID + " .post-comments")[0].insertAdjacentHTML("beforeend", filled);
+        media_file.value = "";
+        comment_text.value = "";
+        $(postID + " .comment-media-file-label p").html("Upload Media");
+    });
 }
 
-function setupSubmitCommentButtons(){
-    var submitCommentButtons = $(".submit-comment-button");
-    for (var i = 0; i < submitCommentButtons.length; i++){
-        submitCommentButtons[i].addEventListener("click", function (event) {
-            event.preventDefault();
-
-            var submitCommentButton = $(this);
-            var postID = "#" + submitCommentButton.attr("data-post-id");
-
-            var comment_text = $(postID + " .submit-comment-text")[0];
-            var media_file = $(postID + " .comment-media-file-button")[0];
-            if (comment_text.value === "" && media_file.files.length === 0)
-                return console.log("Empty form!");
-            var fileName = "";
-            var template_id = "#comment-template";
-            if (media_file.files.length > 0) {
-                template_id = "#image-comment-template";
-                var [file] = media_file.files;
-                fileName = file.name;
-            }
-            var dateString = getDateString();
-            var data = {
-                author: { name: "Naruto Uzumaki", image: "Naruto1.jpg" },
-                comment: { date: dateString, text: comment_text.value, image: fileName }
-            };
-            var template = Handlebars.compile($(template_id).html());
-            var filled = template(data);
-            $(postID + " .post-comments")[0].insertAdjacentHTML("beforeend", filled);
-            media_file.value = "";
-            comment_text.value = "";
-            $(postID + " .comment-media-file-label p").html("Upload Media");
-        });
-    }
-}
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
 
 function getDateString() {
     var currentDate = new Date();
